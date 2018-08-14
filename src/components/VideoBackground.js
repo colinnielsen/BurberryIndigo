9c49d4b75c0aec4940c5842939f5b22ac93bc7fd
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2018-08-14 14:25:53 -0600

    updating repository

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -3,14 +3,13 @@ import videosrc from '../assets/indigo.mp4'
 import Button from './Button.js'
 
 const VideoBackground = (props) => {
-
   return (
     <div className="landing-viewport">
       <video className="landing-viewport__video" playsInline autoPlay loop muted>
-        <source src={videosrc}/>
+        <source src={videosrc} />
       </video>
       <h1>I N D I G O</h1>
-      <Button enterScroll={props.enterScroll}/>
+      <Button enterScroll={props.enterScroll} />
     </div>
   )
 }
